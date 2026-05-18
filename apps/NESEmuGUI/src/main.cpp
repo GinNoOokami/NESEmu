c0035ebabc2b8866c38d8b5a29542c212b2c6ae1
Jeff Padgham <jeff.ookami@gmail.com>
2026-05-18 11:03:28 -0700

    Migrating SDL2 to SDL3

--- apps/NESEmuGUI/src/main.cpp
+++ apps/NESEmuGUI/src/main.cpp
@@ -1,26 +1,27 @@
 #define SDL_MAIN_HANDLED true
-#include <SDL.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
 int main()
 {
-    SDL_Init(SDL_INIT_VIDEO);
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+        return -1;
+    }
 
     SDL_Window* window = SDL_CreateWindow(
         "NESEmuGUI",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
         256,
         240,
         0
         );
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
 
     SDL_Event e;
     bool      quit = false;
     while (!quit) {
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
+            if (e.type == SDL_EVENT_QUIT) {
                 quit = true;
             }
         }
@@ -28,7 +29,7 @@ int main()
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
-        SDL_Delay(0);
+        SDL_Delay(1);
     }
 
     SDL_DestroyRenderer(renderer);
